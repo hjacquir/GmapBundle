de19f3fba4aaa9483cec691cfb16ca4c095400b9
hjacquir <jacquirhatim@gmail.com>
2014-01-26 21:50:04 +0100

    Adding required files

---
+++ HjGmapBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Created by Hatim Jacquir
+ * User: Hatim Jacquir <jacquirhatim@gmail.com>
+ */
+
+namespace Hj\GmapBundle;
+
+use \Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Add a google map on your symfony site
+ */
+class HjGmapBundle extends Bundle
+{
+}
