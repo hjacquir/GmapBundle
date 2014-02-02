63d64d6d6b3f685a5fe3d1598bfbcd7c5c987ca7
hjacquir <jacquirhatim@gmail.com>
2014-02-02 03:15:01 +0100

    add two entities

---
+++ Entity/Adress.php
@@ -0,0 +1,150 @@
+<?php
+
+/* Created by Hatim Jacquir
+ * User: Hatim Jacquir <jacquirhatim@gmail.com>
+ */
+
+namespace Hj\GmapBundle\Entity;
+
+use \Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contain the adress of the place
+ * 
+ * @ORM\Entity()
+ * @ORM\Table(name="gmap_adress")
+ */
+class Adress
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    
+    /**
+     * @ORM\Column(type="string", name="country", length=100)
+     */
+    private $country;
+    
+    /**
+     * @ORM\Column(type="string", name="locality", length=100, nullable=true)
+     */
+    private $locality;
+    
+    /**
+     * @ORM\Column(type="string", name="street_number", length=5, nullable=true)
+     */
+    private $streetNumber;
+    
+    /**
+     * @ORM\Column(type="string", name="street_name", length=255)
+     */
+    private $streetName;
+    
+    /**
+     * @ORM\OneToOne(targetEntity="Hj\GmapBundle\Entity\Location", cascade={"persist"})
+     */
+    private $location;
+    
+    /**
+     * Return the id
+     * 
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     * Return the country name
+     * 
+     * @return string $country
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+    
+    /**
+     * Return the locality name
+     * 
+     * @return string $locality
+     */
+    public function getLocality()
+    {
+        return $this->locality;
+    }
+    
+    /**
+     * Return the street number
+     * 
+     * @return integer $streetNumber
+     */
+    public function getStreetNumber()
+    {
+        return $this->streetNumber;
+    }
+    
+    /**
+     * Return the street name
+     * 
+     * @return string $streetName
+     */
+    public function getStreetName()
+    {
+        return $this->streetName;
+    }
+    
+    /**
+     * @param string $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+    
+    /**
+     * @param string $locality
+     */
+    public function setLocality($locality)
+    {
+        $this->locality = $locality;
+    }
+    
+    /**
+     * @param integer $streetNumber
+     */
+    public function setStreetNumber($streetNumber)
+    {
+        $this->streetNumber = $streetNumber;
+    }
+    
+    /**
+     * @param string $streetName
+     */
+    public function setStreetName($streetName)
+    {
+        $this->streetName = $streetName;
+    }
+    
+    /**
+     * Return an Location object
+     * 
+     * @return Location $location
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+    
+    /**
+     * @param \Hj\GmapBundle\Entity\Location $location
+     */
+    public function setLocation(Location $location)
+    {
+        $this->location = $location;
+    }
+}
---
+++ Entity/Location.php
@@ -0,0 +1,87 @@
+<?php
+
+/* Created by Hatim Jacquir
+ * User: Hatim Jacquir <jacquirhatim@gmail.com>
+ */
+
+namespace Hj\GmapBundle\Entity;
+
+use \Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contains the latitude and longitude of a place
+ * 
+ * @ORM\Entity()
+ * @ORM\Table(name="gmap_location")
+ */
+class Location
+{
+    const CLASS_NAME = __CLASS__;
+    
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    
+    /**
+     * @ORM\Column(type="string", name="latitude", length = 255)
+     */
+    private $lat;
+    
+    /**
+     * @ORM\Column(type="string", name="longitude", length = 255)
+     */
+    private $lng;
+    
+    /**
+     * Return the id
+     * 
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     * Return the latitude of a place
+     * 
+     * @return string
+     */
+    public function getLat()
+    {
+        return $this->lat;
+    }
+    
+    /**
+     * Return the longitude of a place
+     * 
+     * @return string
+     */
+    public function getLng()
+    {
+        return $this->lng;
+    }
+    
+    /**
+     * Define the latitude of a place
+     * 
+     * @param string $lat
+     */
+    public function setLat($lat)
+    {
+        $this->lat = $lat;
+    }
+    
+    /**
+     * Define the longitude of a place
+     * 
+     * @param string $lng
+     */
+    public function setLng($lng)
+    {
+        $this->lng = $lng;
+    }
+}
