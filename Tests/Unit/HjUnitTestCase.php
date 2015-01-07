4c292b2fe847335b8945eddba335beaa0051743f
Hatim Jacquir <jacquirhatim@gmail.com>
2015-01-07 14:26:40 +0100

    increase phpunit time out tests and indent hjunittestcase

--- Tests/Unit/HjUnitTestCase.php
+++ Tests/Unit/HjUnitTestCase.php
@@ -17,80 +17,80 @@ class HjUnitTestCase extends PHPUnit_Framework_TestCase
 {
     /**
      * Assert that object have getter and setter
-     * 
-     * @param object $object            The expected object
-     * @param mixed  $expectedAttribute The attribute concerned which one tests the getter and setter
-     * @param mixed  $expectedValue     The expected value returned by the getter
+     *
+     * @param object $object The expected object
+     * @param mixed $expectedAttribute The attribute concerned which one tests the getter and setter
+     * @param mixed $expectedValue The expected value returned by the getter
      */
     protected function hjAssertThatObjectHaveGetAndSetMethods($object, $expectedAttribute, $expectedValue)
     {
         $appendedAttribute = ucfirst($expectedAttribute);
-       
+
         $setMethod = 'set' . $appendedAttribute;
         $this->hjAssertIfTheMethodExist($object, $setMethod);
-       
+
         $getMethod = 'get' . $appendedAttribute;
         $this->hjAssertIfTheMethodExist($object, $getMethod);
-       
+
         $object->{$setMethod}($expectedValue);
-       
+
         $this->assertSame($expectedValue, $object->{$getMethod}());
     }
-    
-   /**
-    * Assert if the method exist
-    *  
-    * @param Object $object             The expected object
-    * @param string $expectedMethodName The expected method name
-    */
-   protected function hjAssertIfTheMethodExist($object, $expectedMethodName)
-   {
-       $class = new ReflectionClass($object);
-       $method = $class->getMethod($expectedMethodName);
-       $message = 'The method : ' . $expectedMethodName . ' does not exist into ' . $method->class;
-       $this->assertSame($expectedMethodName, $method->name, $message);
-   }
-   
-   /**
-    * Assert that an array has epected key and value
-    * 
-    * @param array $expectedArray
-    * @param mixed $expectedKey
-    * @param mixed $expectedValue
-    */
-   protected function hjAssertIfTheArrayHasKeyAndValue(array $expectedArray, $expectedKey, $expectedValue)
-   {
-       $this->assertArrayHasKey($expectedKey, $expectedArray);
-       $this->assertSame($expectedValue, $expectedArray[$expectedKey]);
-   }
-   
-   /**
-    * Assert that the given object is an instance of URI
-    * 
-    * @param object $expectedObject
-    */
-   protected function hjAssertInstanceOfUri($expectedObject)
-   {
-       $this->assertInstanceOf('Hj\GmapBundle\Services\Uri', $expectedObject);
-   }
-   
-   /**
-    * @return PHPUnit_Framework_MockObject_MockObject
-    */
-   protected function getMockLocationEntity()
-   {
-       return $this->getMockBuilder('Hj\GmapBundle\Entity\Location')
-               ->disableOriginalConstructor()
-               ->getMock();
-   }
-   
-   /**
-    * @return PHPUnit_Framework_MockObject_MockObject
-    */
-   protected function getMockStaticMapEntity()
-   {
-       return $this->getMockBuilder('Hj\GmapBundle\Entity\StaticMap')
-               ->disableOriginalConstructor()
-               ->getMock();
-   }
+
+    /**
+     * Assert if the method exist
+     *
+     * @param Object $object The expected object
+     * @param string $expectedMethodName The expected method name
+     */
+    protected function hjAssertIfTheMethodExist($object, $expectedMethodName)
+    {
+        $class = new ReflectionClass($object);
+        $method = $class->getMethod($expectedMethodName);
+        $message = 'The method : ' . $expectedMethodName . ' does not exist into ' . $method->class;
+        $this->assertSame($expectedMethodName, $method->name, $message);
+    }
+
+    /**
+     * Assert that an array has epected key and value
+     *
+     * @param array $expectedArray
+     * @param mixed $expectedKey
+     * @param mixed $expectedValue
+     */
+    protected function hjAssertIfTheArrayHasKeyAndValue(array $expectedArray, $expectedKey, $expectedValue)
+    {
+        $this->assertArrayHasKey($expectedKey, $expectedArray);
+        $this->assertSame($expectedValue, $expectedArray[$expectedKey]);
+    }
+
+    /**
+     * Assert that the given object is an instance of URI
+     *
+     * @param object $expectedObject
+     */
+    protected function hjAssertInstanceOfUri($expectedObject)
+    {
+        $this->assertInstanceOf('Hj\GmapBundle\Services\Uri', $expectedObject);
+    }
+
+    /**
+     * @return PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getMockLocationEntity()
+    {
+        return $this->getMockBuilder('Hj\GmapBundle\Entity\Location')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @return PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getMockStaticMapEntity()
+    {
+        return $this->getMockBuilder('Hj\GmapBundle\Entity\StaticMap')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
 }
